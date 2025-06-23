b7bf48afe85c510da9d8a6aa3ccfb9fd4e69fbd7
Alejandro Prado <adroprado@outlook.com>
2025-06-23 16:44:50 -0600

    feat(js): add clases y id a html para variables

---
+++ index.js
@@ -0,0 +1,59 @@
+// Planteamiento 1
+/* const $DISPLAY = document.querySelector(".display"),
+  $NUMBERS = document.querySelectorAll(".number"),
+  $OPERATIONS = document.querySelectorAll(".operation"),
+  $DECIMAL_POINT = document.getElementById("decimal-point"),
+  $EQUAL = document.getElementById("equal"),
+  $CLEAR = document.getElementById("clear"),
+  $DELETE = document.getElementById("delete");
+
+console.log(
+  $DISPLAY,
+  $NUMBERS,
+  $OPERATIONS,
+  $DECIMAL_POINT,
+  $EQUAL,
+  $CLEAR,
+  $DELETE
+  ); */
+
+// Planteamiento 2
+function Calculadora(
+  display,
+  numbers,
+  operations,
+  decimalPoint,
+  equal,
+  clear,
+  del
+) {
+  document.addEventListener("click", (e) => {
+    //Realiza operación
+    if (e.target.matches(numbers)) {
+      console.log("Número");
+    }
+
+    //Limpiar display
+    if (e.target.matches(clear)) {
+      console.log("Limpiar display");
+    }
+
+    //Eliminar dígitos del display
+    if (e.target.matches(del)) {
+      console.log("Eliminar dígitos");
+    }
+  });
+}
+
+document.addEventListener(
+  "DOMContentLoaded",
+  Calculadora(
+    ".display",
+    ".number",
+    ".operation",
+    "#decimal-point",
+    "#equal",
+    "#clear",
+    "#delete"
+  )
+);
