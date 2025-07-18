759387cf3542ad0264aacac6d23fe4f4bb53929a
Alejandro Prado <adroprado@outlook.com>
2025-07-18 15:02:08 -0600

    feat(js): add dataattributes al html y ajustes al codigo

--- js/main.js
+++ js/main.js
@@ -20,38 +20,34 @@ let currentNumber = null, // Almacena número actual
   previousNumber = null, // Almacena número anterior
   operator = null, // Almacena operador seleccionado
   result = null, // Resultado de la operación
-  resetScreen = false; // Un booleano que te indicará si la pantalla debe limpiarse antes de mostrar el siguiente número (útil después de un operador o de calcular un resultado).
+  waitForSecondOperator = false;
+//resetScreen = false, // Un booleano que te indicará si la pantalla debe limpiarse antes de mostrar el siguiente número (útil después de un operador o de calcular un resultado).
+
+// Función que actualiza la variable del DOM $DISPLAY
+const updateDisplay = (currentNumber) => {
+  $DISPLAY.textContent = currentNumber;
+};
 
 // Delegación de Eventos
 $CALCULADORA.addEventListener("click", (e) => {
   //Variables que almacenan selectores validos que les aplico el evento "click"
   const NUMBERS = e.target.matches(".number"),
     OPERATIONS = e.target.matches(".operation"),
-    DECIMAL_POINT = e.target.matches("#decimal-point"),
-    EQUAL = e.target.matches("#equal"),
-    CLEAR = e.target.matches("#clear"),
-    DELETE = e.target.matches("#delete");
+    DECIMAL_POINT = e.target.matches(".decimal"),
+    EQUAL = e.target.matches(".equal"),
+    CLEAR = e.target.matches(".clear"),
+    DELETE = e.target.matches(".delete");
 
-  // ! Corroborar las siguientes líneas de código.
-  // Pasando el valor de la etiqueta HTML button(number) como valor a la variable "currentNumber"
-  if (NUMBERS) {
-    currentNumber = e.target.textContent;
-    console.log(currentNumber, typeof currentNumber);
+  console.log(currentNumber);
+
+  // * Programar lógica manejo de eventos
+
+  if (NUMBERS && !waitForSecondOperator) {
+    currentNumber = e.target.dataset.value;
+    waitForSecondOperator = false;
+    console.log(currentNumber);
+    updateDisplay(currentNumber);
   }
-
-  // Pasando el valor de la etiqueta HTML button(operation) como valor a la variable "operator"
-  if (OPERATIONS) {
-    operator = e.target.textContent;
-    console.log(operator, typeof operator);
-  }
-
-  // Mostrando contenido en la variable del DOM $DISPLAY
-  const updateDisplay = (currentNumber) => {
-    $DISPLAY.textContent = currentNumber;
-  };
-  updateDisplay(currentNumber);
-
-  // * Mostrar a la IA mi avnce del proyecto
 });
 
 /* 
